ff775832c8ede5d20de7de97ea9943689c6ecc1d
Jennifer Zuñiga <jenn88zug08@gmail.com>
2024-11-14 13:33:47 -0600

    gh-pages actualizado

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ListaClientes from './Trabajadores/Docente';
+import Calificaciones from './Trabajadores/calificaciones';
+import ListaClientes from './Trabajadores/Docente'; // Assuming ListaClientes is the Docentes component
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ListaClientes/>
+    <div className="container">
+      <Calificaciones />
+      <ListaClientes />
+    </div>
   </React.StrictMode>
 );
 
